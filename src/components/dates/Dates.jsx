655c4249209f6b40c5428a4c555e7ede7db6b68e
YamieM <108412374+YamieM@users.noreply.github.com>
2023-03-17 11:48:51 +0400

    with obj

--- src/components/dates/Dates.jsx
+++ src/components/dates/Dates.jsx
@@ -1,73 +1,87 @@
-import { useCallback, useState } from "react";
+import { useState } from "react";
 import "./style.css";
+import declOfNum from "../../utils/declOfNum";
 
 const Dates = () => {
-  const [firstDate, set1stDate] = useState("1999-01-01");
-  const [secondDate, set2ndDate] = useState("1999");
+  const [dateCurr, setDateCurr] = useState({
+    firstDate: "1999-01-01",
+    secondDate: "1999-01-01",
+    resFirst: "",
+    resSecond: "",
+  });
 
-  const onChange1stDate = (event) => {
-    set1stDate(event.target.value);
+  const onChangeFirstDate = (event) => {
+    setDateCurr((previousState) => {
+      return { ...previousState, firstDate: event.target.value };
+    });
   };
 
-  const onChange2ndDate = (event) => {
-    set2ndDate(event.target.value);
+  const onChangeSecondDate = (event) => {
+    setDateCurr((previousState) => {
+      return { ...previousState, secondDate: event.target.value };
+    });
   };
-
-  const [res1st, setRes1st] = useState("");
-  const [res2nd, setRes2nd] = useState("");
 
   //ЕСЛИ ЗАРАНЕЕ ЗАСУНУТЬ ДАТЫ В ПЕРЕМЕННЫЕ,
   // ТО БУДЕТ КОРОЧЕ И МЕНЕЕ ЗАПУТАННО,
   // НО "НУЖНО ИЗБЕГАТЬ ПРИМИТИВОВ(?)"
 
-  const get1stRes = () => {
-    if (Date.parse(firstDate) > Date.parse(secondDate)) {
-      setRes1st(
-        `${new Date(firstDate).toLocaleString("ru", {
-          dateStyle: "short",
-        })} было позже, чем ${new Date(secondDate).toLocaleString("en-US", {
-          dateStyle: "long",
-        })}`
-      );
-    } else if (Date.parse(firstDate) < Date.parse(secondDate)) {
-      setRes1st(
-        `${new Date(firstDate).toLocaleString("de-DE", {
-          dateStyle: "short",
-        })} было раньше, чем ${new Date(secondDate).toLocaleString("en-US", {
-          dateStyle: "long",
-        })}`
-      );
+  const getFirstRes = () => {
+    if (Date.parse(dateCurr.firstDate) > Date.parse(dateCurr.secondDate)) {
+      setDateCurr((previousState) => {
+        return {
+          ...previousState,
+          resFirst: `${new Date(dateCurr.firstDate).toLocaleString("ru", {
+            dateStyle: "short",
+          })} было позже, чем ${new Date(dateCurr.secondDate).toLocaleString(
+            "en-US",
+            {
+              dateStyle: "long",
+            }
+          )}`,
+        };
+      });
+    } else if (
+      Date.parse(dateCurr.firstDate) < Date.parse(dateCurr.secondDate)
+    ) {
+      setDateCurr((previousState) => {
+        return {
+          ...previousState,
+          resFirst: `${new Date(dateCurr.firstDate).toLocaleString("de-DE", {
+            dateStyle: "short",
+          })} было раньше, чем ${new Date(dateCurr.secondDate).toLocaleString(
+            "en-US",
+            {
+              dateStyle: "long",
+            }
+          )}`,
+        };
+      });
     } else {
-      setRes1st("Даты равны");
+      setDateCurr((previousState) => {
+        return { ...previousState, resFirst: "Даты равны" };
+      });
     }
   };
 
-  const get2ndRes = () => {
-    function declOfNum(numb, text) {
-      numb = Math.abs(numb) % 100;
-      var numb1 = numb % 10;
-      if (numb > 10 && numb < 20) {
-        return `${numb} ${text[2]}`;
-      }
-      if (numb1 > 1 && numb1 < 5) {
-        return `${numb} ${text[1]}`;
-      }
-      if (numb1 === 1) {
-        return `${numb} ${text[0]}`;
-      }
-      return `${numb} ${text[2]}`;
-    }
-    setRes2nd(
-      `Между ${new Date(firstDate).toLocaleString("ko-KR", {
-        dateStyle: "long",
-      })} и ${new Date(secondDate).toLocaleString("en-US", {
-        dateStyle: "long",
-      })}  ${declOfNum(
-        Date.parse(new Date(firstDate)) / 1000 / 60 / 60 / 24 -
-          Date.parse(new Date(secondDate)) / 1000 / 60 / 60 / 24,
-        ["День", "Дня", "Дней"]
-      )}`
-    );
+  const getSecondRes = () => {
+    setDateCurr((previousState) => {
+      return {
+        ...previousState,
+        resSecond: `Между ${new Date(dateCurr.firstDate).toLocaleString(
+          "ko-KR",
+          {
+            dateStyle: "long",
+          }
+        )} и ${new Date(dateCurr.secondDate).toLocaleString("en-US", {
+          dateStyle: "long",
+        })} разница  ${declOfNum(
+          Date.parse(new Date(dateCurr.firstDate)) / 1000 / 60 / 60 / 24 -
+            Date.parse(new Date(dateCurr.secondDate)) / 1000 / 60 / 60 / 24,
+          ["День", "Дня", "Дней"]
+        )}`,
+      };
+    });
   };
 
   return (
@@ -78,7 +92,7 @@ const Dates = () => {
           type="text"
           className="date-input"
           placeholder="1999-01-01"
-          onChange={onChange1stDate}
+          onChange={onChangeFirstDate}
         />
       </div>
       <div>
@@ -87,17 +101,17 @@ const Dates = () => {
           type="text"
           className="date-input"
           placeholder="1999-01-01"
-          onChange={onChange2ndDate}
+          onChange={onChangeSecondDate}
         />
       </div>
-      <button className="dates-button" onClick={get1stRes}>
+      <button className="dates-button" onClick={getFirstRes}>
         Сравнить даты
       </button>
-      <button className="dates-button" onClick={get2ndRes}>
+      <button className="dates-button" onClick={getSecondRes}>
         Разница в днях
       </button>
-      <h1 className="dates-res">{res1st}</h1>
-      <h1 className="dates-res">{res2nd}</h1>
+      <h1 className="dates-res">{dateCurr.resFirst}</h1>
+      <h1 className="dates-res">{dateCurr.resSecond}</h1>
     </div>
   );
 };
