ff7312709b549f7f6863b3ee90697472b26b09d2
YamieM <108412374+YamieM@users.noreply.github.com>
2023-03-21 18:36:03 +0400

    context

--- src/App.js
+++ src/App.js
@@ -11,23 +11,29 @@ import Home from "./pages/Home";
 import Projects from "./pages/Projects";
 import Project from "./pages/Project";
 import Timer from "./pages/Timer";
+import { ThemeProvider } from "./providers/ThemeProvider";
+import Layout from "./components/layout/Layout";
 
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <ScrollToTop />
-        <Navbar />
-        <Routes>
-          <Route path="/timer" element={<Timer />} />
-          <Route path="/" element={<Home />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/project/:id" element={<Project />} />
-          <Route path="/contacts" element={<Contacts />} />
-        </Routes>
-        <Footer />
-      </Router>
-    </div>
+    <ThemeProvider>
+      <Layout>
+        <div className="App">
+          <Router>
+            <ScrollToTop />
+            <Navbar />
+            <Routes>
+              <Route path="/timer" element={<Timer />} />
+              <Route path="/" element={<Home />} />
+              <Route path="/projects" element={<Projects />} />
+              <Route path="/project/:id" element={<Project />} />
+              <Route path="/contacts" element={<Contacts />} />
+            </Routes>
+            <Footer />
+          </Router>
+        </div>
+      </Layout>
+    </ThemeProvider>
   );
 }
 
--- src/components/btnDarkMode/BtnDarkMode.js
+++ src/components/btnDarkMode/BtnDarkMode.js
@@ -1,42 +1,43 @@
-import { useEffect } from "react";
-import { useLocalStorage } from "../../utils/useLocalStorage";
-import detectDarkMode from "../../utils/detectDarkMode";
+// import { useEffect } from "react";
+// import { useLocalStorage } from "../../utils/useLocalStorage";
+// import detectDarkMode from "../../utils/detectDarkMode";
 
 import sun from "./sun.svg";
 import moon from "./moon.svg";
 import "./style.css";
+import useTheme from "../../hooks/useTheme";
 
 const BtnDarkMode = () => {
-  const [darkMode, setDarkMode] = useLocalStorage("darkMode", detectDarkMode);
+  const { isDark, setIsDark } = useTheme();
 
-  useEffect(() => {
-    if (darkMode === "dark") {
-      document.body.classList.add("dark");
-    } else {
-      document.body.classList.remove("dark");
-    }
-  }, [darkMode]);
+  // const [darkMode, setDarkMode] = useLocalStorage("darkMode", detectDarkMode);
 
-  useEffect(() => {
-    window
-      .matchMedia("(prefers-color-scheme: dark)")
-      .addEventListener("change", (event) => {
-        const newColorScheme = event.matches ? "dark" : "light";
-        setDarkMode(newColorScheme);
-      });
-  }, [setDarkMode]);
+  // useEffect(() => {
+  //   if (darkMode === true) {
+  //     setDarkMode(isDark);
+  //   } else {
+  //     setDarkMode(!isDark);
+  //   }
+  // });
+
+  // useEffect(() => {
+  //   window
+  //     .matchMedia("(prefers-color-scheme: dark)")
+  //     .addEventListener("change", (event) => {
+  //       const newColorScheme = event.matches ? true : false;
+  //       setDarkMode(newColorScheme);
+  //     });
+  // }, [setDarkMode]);
 
   const toggleDarkMode = () => {
-    setDarkMode((currentValue) => {
-      return currentValue === "light" ? "dark" : "light";
-    });
+    setIsDark(!isDark);
   };
 
   const btnNormal = "dark-mode-btn";
   const btnActive = "dark-mode-btn dark-mode-btn--active";
   return (
     <button
-      className={darkMode === "dark" ? btnActive : btnNormal}
+      className={isDark === true ? btnActive : btnNormal}
       onClick={toggleDarkMode}
     >
       <img src={sun} alt="Light mode" className="dark-mode-btn__icon" />
